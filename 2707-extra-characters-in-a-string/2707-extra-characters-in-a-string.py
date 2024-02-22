5d5013f099d82370fad448acaf9ddc00df58c635
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-02-22 12:23:14 -0500

    Time: 215 ms (47.55%), Space: 16.6 MB (94.71%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,21 +1,32 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         
-        dp = {}
+        dp = [float('inf')] * (len(s) + 1)
+        dp[0] = 0
         
-        def solve(target):
-            if len(target) == 0:
-                return 0
-            if target in dp:
-                return dp[target]
-            
-            cur_min = float('inf')
-            
+        for i in range(1, len(s) + 1):
             for word in dictionary:
-                if target.startswith(word):
-                    cur_min  = min(solve(target[len(word):]), cur_min)
+                if i >= len(word) and s[i - len(word):i] == word:
+                    dp[i] = min(dp[i], dp[i - len(word)])
+            
+            dp[i] = min(dp[i], dp[i - 1] + 1)
+                
+                
+        return dp[len(s)]
+    
+#         def solve(target):
+#             if len(target) == 0:
+#                 return 0
+#             if target in dp:
+#                 return dp[target]
+            
+#             cur_min = float('inf')
+            
+#             for word in dictionary:
+#                 if target.startswith(word):
+#                     cur_min  = min(solve(target[len(word):]), cur_min)
 
-            cur_min = min(1 + solve(target[1:]), cur_min)
-            dp[target] = cur_min
-            return cur_min
-        return solve(s)
+#             cur_min = min(1 + solve(target[1:]), cur_min)
+#             dp[target] = cur_min
+#             return cur_min
+#         return solve(s)
