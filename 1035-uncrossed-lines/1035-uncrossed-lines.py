0bf42d3cd27c76d3d94b5296e7c9fee2c3d9b1dd
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-02-22 20:18:05 -0500

    Time: 114 ms (83.11%), Space: 17.1 MB (71.59%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -4,24 +4,33 @@ class Solution:
         n = len(nums1)
         m = len(nums2)
         
-        dp = [[-1] * m for _ in range(n)]
+        dp = [[0] * (m+1) for _ in range(n+1)]
         
-        def solve(i, j):
-            if i < 0 or j < 0:
-                return 0
-            
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            count = 0
-            if nums1[i] == nums2[j]:
-                count = 1 + solve(i-1, j-1)
-            
-            else:
-                count = max(solve(i-1, j), solve(i, j-1))
-            dp[i][j] = count
-            
-            return count
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[n][m]
         
-        return solve(n-1, m-1)
+#         def solve(i, j):
+#             if i < 0 or j < 0:
+#                 return 0
+            
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
+            
+#             count = 0
+#             if nums1[i] == nums2[j]:
+#                 count = 1 + solve(i-1, j-1)
+            
+#             else:
+#                 count = max(solve(i-1, j), solve(i, j-1))
+#             dp[i][j] = count
+            
+#             return count
+        
+#         return solve(n-1, m-1)
         
