89fca24910b3940207e09f7c60cddc292b732951
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-02-23 20:01:43 -0500

    Time: 28 ms (97.84%), Space: 16.6 MB (82.63%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,16 +1,22 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def best_score_diff(i,j):
+        dp = [[-1] * len(nums) for _ in range(len(nums))]
+        
+        def solve(i,j):
             # invalid state
             if i > j:
                 return 0
-            # whoever reaches here gets this number
+            
             if i == j:
                 return nums[i]
             
-            # choose best of both choices
-            score_diff_i = nums[i] - best_score_diff(i+1,j) # chose i
-            score_diff_j = nums[j] - best_score_diff(i,j-1) # chose j
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            score_diff_i = nums[i] - solve(i+1,j) 
+            score_diff_j = nums[j] - solve(i,j-1)
+            
+            dp[i][j] = max(score_diff_i,score_diff_j)
             return max(score_diff_i,score_diff_j)
         
-        return best_score_diff(0,len(nums)-1) >= 0
+        return solve(0,len(nums)-1) >= 0
