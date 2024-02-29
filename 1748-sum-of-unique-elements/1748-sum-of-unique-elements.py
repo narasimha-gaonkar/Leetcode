ff458aa144be0296162fc91cfa5ef8324504516c
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-02-28 20:57:35 -0500

    Time: 26 ms (98.87%), Space: 16.7 MB (42.84%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -2,14 +2,18 @@ class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
         
         nums_map = defaultdict(int)
-        
+        sums = 0
         for i in nums:
             nums_map[i] +=1
-        sums = 0
+            if nums_map[i]  == 2:
+                sums -= i
+            elif nums_map[i] == 1:
+                sums += i
+        return max(sums, 0)
+#         sums = 0
         
-        
-        for ele in nums_map:
-            if nums_map[ele] == 1:
-                sums += ele
-        return sums
+#         for ele in nums_map:
+#             if nums_map[ele] == 1:
+#                 sums += ele
+#         return sums
         
