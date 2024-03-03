d00f5c3272f07a93af2209188ac0cd973cc6e870
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-02 21:49:55 -0500

    Time: 918 ms (33.33%), Space: 17.1 MB (55.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,9 +4,7 @@ class Solution:
         dp = [-1] * (amount+1) 
         dp[0] = 0
         
-        # for i in range(amount+1):
-        #     dp[i][0] = 0
-        
+
         for i in range(1, amount+1):
             min_coins = float('inf')
             for coin in coins:
@@ -15,8 +13,8 @@ class Solution:
                 
             dp[i] = min_coins if min_coins != float('inf') else -1
                 
-                
         return dp[amount]
+    
         #Recursive Approach
         '''
         dp = [-1] * (amount+1) 
