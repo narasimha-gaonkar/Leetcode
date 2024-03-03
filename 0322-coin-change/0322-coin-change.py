a5fcc3a6857fd1448248ec0c0ef596371cee968d
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-02 20:27:43 -0500

    Time: 991 ms (26.65%), Space: 22.8 MB (21.70%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,24 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [-1] * (amount+1) 
+        if amount == 0:
+            return 0
+        def solve(n):
+            if n == 0:
+                return 0
+            
+            if dp[n] != -1:
+                return dp[n]
+            
+            min_s = float(inf)
+            
+            for coin in coins:
+                
+                if n - coin >= 0:
+                    min_s = min(min_s, 1 + solve(n - coin))
+            dp[n] = min_s
+            return min_s
+        solve(amount)
+        print(dp)
+        return -1 if dp[amount] == float(inf) else dp[amount]
+                
