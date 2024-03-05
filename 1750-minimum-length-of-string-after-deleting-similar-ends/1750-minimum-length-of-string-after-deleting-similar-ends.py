41606f58521b9a2883a563de69f0b5196ddc57f2
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-05 09:43:00 -0500

    Time: 70 ms (57.35%), Space: 17.4 MB (53.55%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -8,6 +8,7 @@ class Solution:
                 
             while i < j and s[i] == s[i+1]:
                 i += 1
+            
             while i<j and s[j] == s[j - 1]:
                 j -= 1
             
