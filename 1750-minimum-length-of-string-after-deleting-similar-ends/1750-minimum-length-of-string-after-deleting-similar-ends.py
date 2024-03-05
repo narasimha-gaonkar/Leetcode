1edabceaad66af865c3ecaff1bbcd6a653ec32be
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-05 09:42:43 -0500

    Time: 51 ms (99.53%), Space: 17.5 MB (53.55%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -4,29 +4,16 @@ class Solution:
         i = 0 
         j = len(s) - 1
         
-        while i < j:
-            
-            if s[i] == s[j]:
-                if j - i == 1:
-                    return 0
+        while i < j and s[i] == s[j]:
                 
-                while i<j:
-                    if s[i] == s[i+1]:
-                        i += 1
-                    else:
-                        break
-                while i<j:
-                    if s[j] == s[j - 1]:
-                        j -= 1
-                    else:
-                        break
+            while i < j and s[i] == s[i+1]:
                 i += 1
+            while i<j and s[j] == s[j - 1]:
                 j -= 1
-                if i > j:
-                    return 0
-            else:
-                return j - i + 1
+            
+            i += 1
+            j -= 1
                 
-        return 1
+        return j - i + 1 if i <= j else 0
             
         
