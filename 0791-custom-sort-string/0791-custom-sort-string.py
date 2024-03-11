f48b7e1445cfcfc6a4f11f413546eb7ea88a1987
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-11 14:22:12 -0400

    Time: 33 ms (73.85%), Space: 16.5 MB (61.09%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -5,16 +5,16 @@ class Solution:
         for char in s:
             hmap[char] = hmap.get(char, 0) + 1
         res = ''
+        
         for seq in order:
             if seq in hmap:
                 res = res + seq * hmap[seq]
                 
                 del hmap[seq]
-        # print(res)
+
         for char in hmap:
             res = res + char * hmap[char]
-        # print(hmap)
-        # print(res)
+
         
         return res
             
