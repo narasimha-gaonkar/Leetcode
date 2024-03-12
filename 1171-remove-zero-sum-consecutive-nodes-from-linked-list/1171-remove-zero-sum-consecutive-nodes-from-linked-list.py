e8a6b1274ea2c3e8a5e7ffa285b830071aa9dd97
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-11 23:27:45 -0400

    Time: 101 ms (5.58%), Space: 17 MB (29.48%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -6,47 +6,24 @@
 class Solution:
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
         nums = []
-        
-
         cur = head
-        is_neg_exist = False
+        
         while cur:
-            
-            # if cur.val < 0 and nums:
             temp = cur.val
             j = len(nums) - 1
-            # print('18, ', temp, nums)
             deleted = False
             while j >=0:
                 temp = temp + nums[j]
-                # print('Line 21', temp, j)
                 if temp == 0:
                     deleted = True
                     nums = nums[:j]
-                    # print('Line 24', nums)
                     break
                 j -= 1
             if not deleted and cur.val != 0:
                 nums.append(cur.val)
                     
-            # elif cur.val > 0 and nums:
-            #     temp = cur.val
-            #     j = len(nums) - 1
-            #     print('30, ', temp, nums)
-            #     while j >=0:
-            #         temp = temp + nums[j]
-            #         print('Line 33', temp, j)
-            #         if temp == 0:
-            #             nums = nums[:j]
-            #             print('Line 36', nums)
-            #             break
-            #         j -= 1
-                
-                
-                # nums.append(cur.val)
-            
-            
             cur =  cur.next
+        
         if not nums:
             return None
         
