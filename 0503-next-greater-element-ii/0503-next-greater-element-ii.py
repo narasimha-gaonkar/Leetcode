0a002febbaf9e89009a883671e7b4fce2bba5c7b
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-13 17:14:06 -0400

    Time: 139 ms (94.40%), Space: 18.4 MB (62.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,29 +1,14 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         
-        
-        p = 1
-        n = len(nums)
+        stack, n = [], len(nums)
         res = [-1] * n
-        for i in range(n):
-            # print('9 Line ', i, nums[i])
-            p = i + 1
-            if p == n:
-                p = 0
-            while p < n and p!=i:
-                # print('11 Line', nums[p] , nums[i])
-                
-                if nums[p] > nums[i]:
-                    res[i] = nums[p]
-                    break
-                
-                if p == n-1:
-                    # print('17 Line', p)
-                    p = 0
-                else:
-                    p += 1
-                # print('Line 21->', p)
-            
+
+        for i in range(n*2):
+            idx = i % n
+            while stack and nums[stack[-1]] < nums[idx]:
+                res[stack.pop()] = nums[idx]
+            stack.append(idx)
         return res
             
             
