71d285ab7cc344f57ba334927b7fa87b3918e40d
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-13 09:51:45 -0400

    Time: 34 ms (93.23%), Space: 16.6 MB (59.17%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -3,7 +3,7 @@ class Solution:
     def pivotInteger(self, n: int) -> int:
         
         root = math.sqrt((n * ( n + 1)) / 2)
-        print(root)
+        # print(root)
         if not root.is_integer(): return -1
         
         for i in range(n, -1, -1):
