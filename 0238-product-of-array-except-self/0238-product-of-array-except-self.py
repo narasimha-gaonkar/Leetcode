07e47180ad476a965bff17c903b3c346db907918
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-14 20:27:06 -0400

    Time: 173 ms (32.55%), Space: 23.8 MB (77.62%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,38 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        product = 1
-        count_zero = 0
-        for num in nums:
-            if num != 0:
-                product *= num
-            else:
-                count_zero += 1
-        
-        res = [0] * len(nums)
+        product_pre = product_post =  1
+        n = len(nums)
+        res = [1] * n
+        postfix_sum = [1] * n
+        prefix_sum = [1] * n
 
-        for i in range(len(nums)):
-            if nums[i] != 0 and count_zero == 0:
-                res[i] = int(product / nums[i])
-            elif nums[i] == 0 and count_zero == 1:
-                res[i] = product
+        
+        for i in range(n):
+            prefix_sum[i] = product_pre
+            product_pre *= nums[i]
+            
+            postfix_sum[n - i - 1] = product_post
+            product_post *= nums[n - i - 1]
+            
+        for i in range(n):
+            res[i] = postfix_sum[i] * prefix_sum[i]
+            
         return res
+
+
+#         for num in nums:
+#             if num != 0:
+#                 product *= num
+#             else:
+#                 count_zero += 1
+        
+#         res = [0] * len(nums)
+
+#         for i in range(len(nums)):
+#             if nums[i] != 0 and count_zero == 0:
+#                 res[i] = int(product / nums[i])
+#             elif nums[i] == 0 and count_zero == 1:
+#                 res[i] = product
+#         return res
         
