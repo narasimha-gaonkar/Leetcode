76bc190c401f501cdb4fdef336c01a4dd5736058
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-14 20:31:13 -0400

    Time: 165 ms (64.62%), Space: 23.9 MB (54.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -13,20 +13,4 @@ class Solution:
             res[i] = res[i] * product_post
             product_post *= nums[i]
         return res
-
-
-#         for num in nums:
-#             if num != 0:
-#                 product *= num
-#             else:
-#                 count_zero += 1
-        
-#         res = [0] * len(nums)
-
-#         for i in range(len(nums)):
-#             if nums[i] != 0 and count_zero == 0:
-#                 res[i] = int(product / nums[i])
-#             elif nums[i] == 0 and count_zero == 1:
-#                 res[i] = product
-#         return res
         
