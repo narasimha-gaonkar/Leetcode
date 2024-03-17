277479ed41c3cda3b696f3668e53c1fbe87e02e4
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-17 09:16:49 -0400

    Time: 38 ms (50.06%), Space: 16.7 MB (12.85%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,43 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        visited = {}
+        
+        m = len(board)
+        n = len(board[0])
+        
+        def getGridValues(x, y):
+            if x < m and y < n and x >= 0 and y >= 0:
+                if (x, y) in visited:
+                    return visited[(x, y)]
+                return board[x][y]
+            return 0
+        
+        
+        for i in range(m):
+            for j in range(n):
+                visited[(i, j)] = board[i][j]
+                summ = 0
+                for k in range(-1, 2):
+                    for l in range(-1, 2):
+                        summ += getGridValues(i+k, j+l)
+                
+                summ -= board[i][j]
+                # print(summ, i, j)
+                if board[i][j] == 0 and summ == 3:
+                    board[i][j] = 1
+                elif board[i][j] == 1 and summ < 2:
+                    
+                    board[i][j] = 0
+                    # print('line 33',i, j, board[i][j])
+                elif board[i][j] == 1 and (summ == 2 or summ == 3):
+                    board[i][j] = 1
+                    # print('line 36',i, j, board[i][j])
+                elif board[i][j] == 1 and summ > 3:
+                    board[i][j] = 0
+                else:
+                    board[i][j] = 0
+        return board
+        
