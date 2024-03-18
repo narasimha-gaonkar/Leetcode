966762e172a521ab7eb08cfbdc94bf9e11a5e5de
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-03-17 23:31:34 -0400

    Time: 75 ms (59.32%), Space: 19.8 MB (79.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -5,37 +5,61 @@ class Solution:
         res = []
         x, y = newInterval
         
-        while intervals:
-            start, end = intervals.pop(0)
+        inserted = False
+        for interval in intervals:
             
-            if not res and y < start:
-                res.append(newInterval)
-                res.append([start, end])
-                res.extend(intervals)
-                return res
-                           
-            if y < start:
-                res.append(newInterval)
-                res.append([start, end])
-                res.extend(intervals)
-                return res
-            elif x <= end:
-                maxx = max(end, y)
-                minn = min(start, x)
-                
-                while intervals and  maxx >= intervals[0][0]:
-                    
-                    maxx = max(intervals.pop(0)[1], maxx)
-                    
-                res.append([minn, maxx])
-                
-                res.extend(intervals)
-                return res
-    
+            if interval[1] < x:
+                res.append(interval)
+            elif interval[0] > y:
+                if not inserted:
+                    res.append([x, y])
+                    inserted = True
+                res.append(interval)
             else:
-            
-                res.append([start, end])
-
-        res.append(newInterval)
+                x = min(x, interval[0])
+                y = max(y, interval[1])
+        
+        if not inserted:
+            res.append([x, y])
         return res
+        
+        
+#         if not intervals:
+#             return [newInterval]
+#         res = []
+#         x, y = newInterval
+        
+#         while intervals:
+#             start, end = intervals.pop(0)
+            
+#             if not res and y < start:
+#                 res.append(newInterval)
+#                 res.append([start, end])
+#                 res.extend(intervals)
+#                 return res
+                           
+#             if y < start:
+#                 res.append(newInterval)
+#                 res.append([start, end])
+#                 res.extend(intervals)
+#                 return res
+#             elif x <= end:
+#                 maxx = max(end, y)
+#                 minn = min(start, x)
+                
+#                 while intervals and  maxx >= intervals[0][0]:
+                    
+#                     maxx = max(intervals.pop(0)[1], maxx)
+                    
+#                 res.append([minn, maxx])
+                
+#                 res.extend(intervals)
+#                 return res
+    
+#             else:
+            
+#                 res.append([start, end])
+
+#         res.append(newInterval)
+#         return res
             
