2a88c7a83b950a6417f2002e012f900f25b84307
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-05 11:04:14 -0400

    Time: 30 ms (93.49%), Space: 16.5 MB (46.83%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -3,11 +3,10 @@ class Solution:
         
         stack = []
         
-        print(ord('A'),ord('Z'), ord('a'), ord('z')) 
-        
         for letter in s:
             if stack and letter.lower() == stack[-1].lower() and ord(letter) != ord(stack[-1]):
                 stack.pop()
             else:
                 stack.append(letter)
+                
         return ''.join(stack)
