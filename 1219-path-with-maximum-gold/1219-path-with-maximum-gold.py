2353a7a59819d83bf60c9520942b127eedcbd7fd
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-11 16:55:18 -0400

    Time: 2715 ms (58.69%), Space: 16.7 MB (28.08%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -2,16 +2,21 @@ class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
             
             steps = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            visited = set()
+            
+            m = len(grid)
+            n = len(grid[0])
+            max_gold = 0
+            visited = [[False] * n for _ in range(m)]
+            
             def val(i,j):
-                if i < 0 or j < 0 or i >= m or j >= n or (i, j) in visited:
+                if i < 0 or j < 0 or i >= m or j >= n or visited[i][j]:
                     return False
                 return grid[i][j]!=0
             
             def solve(i, j):
                 tmp_max = 0
                 
-                visited.add((i, j))
+                visited[i][j] = True
                 
                 for x, y in steps:
                     
@@ -20,19 +25,14 @@ class Solution:
                     
                     if val(new_x, new_y):
                         tmp_max = max(tmp_max, grid[new_x][new_y] + solve(new_x, new_y))
-                visited.remove((i, j))
+                
+                visited[i][j] = False
+                
                 return tmp_max
-
-            
-            m = len(grid)
-            n = len(grid[0])
-            max_gold = 0
             
             for i in range(m):
                 for j in range(n):
-                    
-                    if grid[i][j] !=0 :
-                        visited = set()
+                    if grid[i][j] !=0 and not visited[i][j]:
                         max_gold = max(max_gold, grid[i][j]  + solve(i, j))
             return max_gold
                     
