5f174ad8321d7f224e8a300aa2be90dca20f1761
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-11 20:19:10 -0400

    Time: 360 ms (23.95%), Space: 19.3 MB (94.85%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,22 +1,23 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         
-        
-        def subarrayWithMaxKDistinct(n_ele):
+        def kdist(temp_k):
+            freq = {}
             count = 0
-            freq = defaultdict(int)
-            start = 0
-            
-            for end in range(len(nums)):
-                
-                freq[nums[end]] += 1
-                
-                while len(freq) > n_ele:
-                    freq[nums[start]] -= 1
-                    if  freq[nums[start]] == 0:
-                        del freq[nums[start]]
-                    start += 1
-                count += end - start + 1
-                
+            l = 0
+            for r in range(len(nums)):
+
+                freq[nums[r]] = freq.get(nums[r], 0) + 1
+                # print(freq, count)
+                while len(freq) > temp_k:
+
+                    freq[nums[l]] = freq[nums[l]] - 1
+                    if freq[nums[l]] == 0:
+                        del freq[nums[l]]
+                    l += 1
+                count += r - l + 1
+
             return count
-        return subarrayWithMaxKDistinct(k) - subarrayWithMaxKDistinct(k - 1)
+        return kdist(k) - kdist(k-1)
+                
+        
