c7d8313c04735bb0a83a20de7faf7c64518fdda4
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-14 19:25:08 -0400

    Time: 86 ms (73.97%), Space: 21.4 MB (34.54%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,35 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        freq = Counter(nums)
+        
+        unique_nums = list(freq.keys())
+        n = len(unique_nums)
+        print(n)
+        k = n - k
+        # print(freq)
+        
+        def quickSelect(l , r):
+            pivot = freq[unique_nums[r]]
+            p = l
+            
+            for i in range(l, r):
+                if freq[unique_nums[i]] <= pivot:
+                    unique_nums[p], unique_nums[i] = unique_nums[i], unique_nums[p]
+                    p += 1
+            unique_nums[r], unique_nums[p] = unique_nums[p], unique_nums[r]
+            
+            if p > k: return quickSelect(l, p-1)
+            elif p < k: return quickSelect(p+1, r)
+            else:
+                return
+        
+        quickSelect(0, n -1)
+        return unique_nums[k:]
+        
+            
+            
+            
+            
+            
+        
