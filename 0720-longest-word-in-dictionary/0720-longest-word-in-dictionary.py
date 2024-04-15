b45d0ee6ed4f8ae1c7d5ada83577106872022355
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-15 15:12:34 -0400

    Time: 79 ms (73.93%), Space: 17 MB (93.06%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -8,15 +8,21 @@ class Solution:
         compare = set()
         compare.add('')
         
+        max_len_word = ''
+        
         max_len = 0
         
         for word in words:
             if word[:-1] in compare:
-                if len(word) >= max_len:
+                if len(word) > max_len:
+                    max_len_word = word
                     max_len = len(word)
-                    res[word] = len(word)
+                elif len(word) == max_len and word < max_len_word:
+                    max_len_word = word
+                    max_len = len(word)
+                    
                 compare.add(word)
-        
+        return max_len_word
         if not res:
             return ''
         res = list(filter(lambda x: x[1] == max_len, res.items()))
