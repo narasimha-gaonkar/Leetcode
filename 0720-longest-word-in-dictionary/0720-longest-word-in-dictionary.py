20c5dd032472b3ae891ca79ba6c2afa10710598a
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-15 15:12:58 -0400

    Time: 71 ms (89.40%), Space: 17.2 MB (44.70%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -3,8 +3,6 @@ class Solution:
         
         words.sort(key=lambda s: len(s))
         
-        res = {}
-        
         compare = set()
         compare.add('')
         
@@ -23,12 +21,4 @@ class Solution:
                     
                 compare.add(word)
         return max_len_word
-        if not res:
-            return ''
-        res = list(filter(lambda x: x[1] == max_len, res.items()))
-        
-        if len(res) == 1:
-            return res[0][0]
-        
-        return sorted(res)[0][0]
         
