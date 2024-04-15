56bcd6d9c37abf0465060e4b91914b205d1d92ae
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-15 10:59:14 -0400

    Time: 34 ms (67.51%), Space: 16.8 MB (5.06%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -6,38 +6,32 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         
-        dp = [[False] * n for _ in range(m)]
+        visited = [[False] * n for _ in range(m)]
         x, y = 0, 0
         k = 0
         res = []
         count = 0
-        
-        def val(i, j):
-            if i < 0 or j < 0 or i >= m or j >= n or dp[i][j]:
-                return False
-            return True
-        
-        while True:
+
+        while count < m * n:
             count += 1
-            # print(count, x, y)
             
-            dp[x][y] = True
+            visited[x][y] = True
             res.append(matrix[x][y])
-            if count == m * n:
-                return res
             
             dx, dy = steps[k]
             
-            if val(x+dx, y+dy):
-                x = x + dx
-                y = y + dy
+            nx = x + dx
+            ny = y + dy
+            
+            if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
+                x, y = nx, ny
             else:
-                k += 1
-                if k == 4:
-                    k = 0
+                k = (k + 1) % 4
+                
                 dx, dy = steps[k]
                 x = x + dx
                 y = y + dy
+        return res
             
             
             
