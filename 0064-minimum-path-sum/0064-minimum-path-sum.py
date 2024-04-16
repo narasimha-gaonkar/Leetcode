97d67f83a69b0726b2a253e4f72feea6467c77de
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-16 15:51:09 -0400

    Time: 95 ms (19.36%), Space: 18.5 MB (31.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -5,25 +5,46 @@ class Solution:
         
         dp = [[-1] * n for _ in range(m)]
         
-        def solve(i, j):
-            
-            if i == 0 and j == 0:
-                return grid[i][j]
-            
-            if i < 0 or j < 0 or i > m or j > n:
-                return float(inf)
-            
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            up = grid[i][j] + solve(i-1, j) 
-            left = grid[i][j] + solve(i, j - 1)
-            
-            dp[i][j] = min(up, left)
-            
-            return dp[i][j]
+        dp[0][0] = grid[0][0]
         
-        return solve(m-1, n-1)
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                up = grid[i][j]
+                left = grid[i][j]
+                if i > 0: 
+                    up += dp[i-1][j] 
+                else:
+                    up = float(inf)
+                if j > 0: 
+                    left += dp[i][j-1] 
+                else:
+                    left = float(inf)
+            
+                dp[i][j] = min(up, left)
+        # print(dp)
+        return dp[m-1][n-1]        
+    
+#         def solve(i, j):
+            
+#             if i == 0 and j == 0:
+#                 return grid[i][j]
+            
+#             if i < 0 or j < 0 or i > m or j > n:
+#                 return float(inf)
+            
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
+            
+#             up = grid[i][j] + solve(i-1, j) 
+#             left = grid[i][j] + solve(i, j - 1)
+            
+#             dp[i][j] = min(up, left)
+            
+#             return dp[i][j]
+        
+#         return solve(m-1, n-1)
 
         
             
