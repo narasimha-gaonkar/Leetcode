becfca095e2a3414ac023b9ce1d28de83b1d5b15
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-17 19:55:01 -0400

    Time: 172 ms (78.03%), Space: 19 MB (53.45%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
@@ -3,20 +3,18 @@ class Solution:
         
         max_length_str = ''
         n = len(s)
+        dictionary = set(dictionary)
         
         for word_c in dictionary:
             i = 0
             k = len(word_c)
-            # print(k)
             for letter in word_c:
                 while i < n:
-                    # print(letter, s[i])
                     if letter == s[i]:
                         k -= 1
                         i += 1
                         break
                     i += 1
-            # print(k)
             if k == 0 and len(word_c) > len(max_length_str):
                     max_length_str = word_c
             elif k == 0 and len(word_c) == len(max_length_str):
