e9f27d92425e7fdb0dfe220e1b1da1e134a53397
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-04-20 12:47:25 -0400

    Time: 1072 ms (15.00%), Space: 44.5 MB (13.50%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -0,0 +1,41 @@
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        
+        res = []
+        m = len(land)
+        n = len(land[0])
+        steps = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        dp = [ [False] * n for _ in range(m)]
+        
+        def val(i, j):
+            if 0 <= i < m and 0 <= j < n and land[i][j] == 1 and dp[i][j] == False:
+                return True
+            return False
+        
+        def solve(i, j, max_i, max_j):
+            
+            dp[i][j] = True
+            
+            max_i = max(max_i, i)
+            max_j = max(max_j, j)
+            
+            for dx, dy in steps:
+                if val(i + dx, j + dy):
+                    tmp_max_x, tmp_max_y =  solve(i + dx, j + dy, max_i, max_j)
+                    
+                    max_i = max(max_i, tmp_max_x)
+                    max_j = max(max_j, tmp_max_y)
+                    
+            return [max_i, max_j]
+                    
+        for i in range(m):
+            for j in range(n):
+                if land[i][j] == 1 and dp[i][j] == False:
+                    tmp = [i, j]
+                    tmp.extend(solve(i, j, i, j))
+                    res.append(tmp)
+        return res
+            
+            
+            
