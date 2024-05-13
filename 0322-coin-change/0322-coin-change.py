1d97325fa9b382c3bb4ee17c3883ccabe0fd35ad
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-05-13 16:04:27 -0400

    Time: 901 ms (34.77%), Space: 37 MB (14.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,45 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
-        dp = [-1] * (amount+1) 
-        dp[0] = 0
-        
-
-        for i in range(1, amount+1):
-            min_coins = float('inf')
-            for coin in coins:
-                if i - coin >= 0 and dp[i - coin] != -1:
-                     min_coins = min(min_coins, dp[i - coin] + 1)
-                
-            dp[i] = min_coins if min_coins != float('inf') else -1
-                
-        return dp[amount]
-    
-        #Recursive Approach
-        '''
-        dp = [-1] * (amount+1) 
-        
-        if amount == 0:
-            return 0
-        
-        def solve(n):
-            if n == 0:
+        @cache
+        def solve(amount):
+            
+            if amount == 0:
                 return 0
             
-            if dp[n] != -1:
-                return dp[n]
+            if amount <= 0:
+                return float(inf)
             
-            min_s = float(inf)
             
+            steps = float(inf)
             for coin in coins:
+                steps = min(steps, 1 + solve(amount - coin))
+            
+            return steps
+        res = solve(amount)  
+        return res if res != float(inf) else -1
                 
-                if n - coin >= 0:
-                    min_s = min(min_s, 1 + solve(n - coin))
-            dp[n] = min_s
-            return min_s
-        solve(amount)
-
-        return -1 if dp[amount] == float(inf) else dp[amount]
+                
+            
         
-        '''
-                
