e7d58b73fd2581d6b20278668b95293644a4e6d2
Narasimha Gaonkar <narasimhagaonkar987@gmail.com>
2024-05-12 20:53:37 -0400

    Time: 178 ms (83.56%), Space: 17.8 MB (17.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -5,20 +5,17 @@ class Solution:
         
         visited = [False] * n
         
+        def solve(node):
+            visited[node] = True
+            for j in range(n):
+                if isConnected[node][j] == 1 and not visited[j]:
+                    solve(j)
+        
         province = 0
         for i in range(n):
             if not visited[i]:
                 province += 1
-                
-                queue = [i]
-                
-                while queue:
-                    node = queue.pop(0)
-                    
-                    for j in range(n):
-                        if isConnected[node][j] == 1 and not visited[j]:
-                            queue.append(j)
-                            visited[j] = True
+                solve(i)
         
         return province
             
